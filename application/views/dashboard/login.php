541e61f0806801a6037b49e5028fd5965016cdf0
DIS-Jakarta <tom.maildesk@gmail.com>
2016-07-31 15:39:32 +0700

    login page
    login page

--- application/views/dashboard/login.php
+++ application/views/dashboard/login.php
@@ -3,7 +3,7 @@
   
 <head>
     <meta charset="utf-8">
-    <title>Login - Bootstrap Admin Template</title>
+    <title>PGN - SIM Login</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes"> 
@@ -20,53 +20,8 @@
 </head>
 
 <body>
-	
-	<div class="navbar navbar-fixed-top">
-	
-	<div class="navbar-inner">
-		
-		<div class="container">
-			
-			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</a>
-			
-			<a class="brand" href="index.html">
-				Bootstrap Admin Template				
-			</a>		
-			
-			<div class="nav-collapse">
-				<ul class="nav pull-right">
-					
-					<li class="">						
-						<a href="signup.html" class="">
-							Don't have an account?
-						</a>
-						
-					</li>
-					
-					<li class="">						
-						<a href="index.html" class="">
-							<i class="icon-chevron-left"></i>
-							Back to Homepage
-						</a>
-						
-					</li>
-				</ul>
-				
-			</div><!--/.nav-collapse -->	
-	
-		</div> <!-- /container -->
-		
-	</div> <!-- /navbar-inner -->
-	
-</div> <!-- /navbar -->
 
-
-
-<div class="account-container">
+<div class="account-container" style ="padding-top:5%;width: 20%;margin: auto;">
 	
 	<div class="content clearfix">
 		
@@ -90,13 +45,7 @@
 				
 			</div> <!-- /login-fields -->
 			
-			<div class="login-actions">
-				
-				<span class="login-checkbox">
-					<input id="Field" name="Field" type="checkbox" class="field login-checkbox" value="First Choice" tabindex="4" />
-					<label class="choice" for="Field">Keep me signed in</label>
-				</span>
-									
+			<div class="login-actions">				
 				<button class="button btn btn-success btn-large">Sign In</button>
 				
 			</div> <!-- .actions -->
@@ -108,13 +57,6 @@
 	</div> <!-- /content -->
 	
 </div> <!-- /account-container -->
-
-
-
-<div class="login-extra">
-	<a href="#">Reset Password</a>
-</div> <!-- /login-extra -->
-
 
 <script src="js/jquery-1.7.2.min.js"></script>
 <script src="js/bootstrap.js"></script>
