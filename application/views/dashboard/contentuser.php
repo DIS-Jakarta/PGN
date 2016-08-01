047ed442bfaa121d2ee1f4c345c28d3936602f8e
DIS-Jakarta <tom.maildesk@gmail.com>
2016-08-01 08:07:46 +0700

    1 agustus 2016
    change page content user

--- application/views/dashboard/contentuser.php
+++ application/views/dashboard/contentuser.php
@@ -1,20 +1,18 @@
-<div class="main" style="border-bottom:0px;padding-top:20px;">
+<div class="main" style="border-bottom:0px;padding-top:20px;padding-bottom:0;">
   <div class="main-inner">
     <div class="container">
       <div class="row">
-	  <div class="container" style="width:50%;height:50%;overflow-x:auto;overflow-y: hidden;white-space: nowrap;">
 	  <div style="float:left;">
-		<div style="margin: auto;width: 20%;"><img src="<? echo site_url();?>static/images/pgn.png"></div>
+		<div><img src="<? echo site_url();?>static/images/silhouette-user.png" class="content-icon"></div>
 		<div style="margin: auto;width: 38%;text-align-last: center;"><p style="font-size:16px;">Selamat datang di website Sistem Informasi Manajemen PGN</p></div>
 		</div>
 		 <div style="float:left;">
-		<div style="margin: auto;width: 20%;"><img src="<? echo site_url();?>static/images/pgn.png"></div>
-		<div style="margin: auto;width: 38%;text-align-last: center;"><p style="font-size:16px;">Selamat datang di website Sistem Informasi Manajemen PGN</p></div>
+		<div><img src="<? echo site_url();?>static/images/silhouette-user.png" class="content-icon"></div>
+		<div style="text-align-last: center;"><p style="font-size:16px;">Selamat datang di website Sistem Informasi Manajemen PGN</p></div>
 		</div>
 		 <div>
-		<div style="margin: auto;width: 20%;"><img src="<? echo site_url();?>static/images/pgn.png"></div>
-		<div style="margin: auto;width: 38%;text-align-last: center;"><p style="font-size:16px;">Selamat datang di website Sistem Informasi Manajemen PGN</p></div>
-		</div>
+		<div><img src="<? echo site_url();?>static/images/silhouette-user.png" class="content-icon"></div>
+		<div style="text-align-last: center;"><p style="font-size:16px;">Selamat datang di website Sistem Informasi Manajemen PGN</p></div>
 		</div>
       <!-- /row --> 
     </div>
